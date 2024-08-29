3500fcccceaa8494fd46ae94a5858b5325a0a8c0
ashmystic <andrew.scott.herman@gmail.com>
2024-08-29 18:42:59 -0400

    add debug statement to service worker caching

--- js/service_worker.js
+++ js/service_worker.js
@@ -12,16 +12,17 @@ var urlsToCache = [
   '/fonts/obelisk-mxvv-font/ObeliskMxvvRegular-RGj6.ttf',
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(urlsToCache);
+        })
+        .catch(error => {
+          console.error('Failed to cache resources:', error);
+        })
+    );
+  });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
